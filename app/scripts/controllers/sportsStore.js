b751556e5feaeacd7ada8ce9e19ae0a6deffb247
Guillermo Sanchez <Guillermo Sanchez>
2016-05-01 16:00:32 -0600

    changes

--- app/scripts/controllers/sportsStore.js
+++ app/scripts/controllers/sportsStore.js
@@ -1,5 +1,4 @@
 ﻿angular.module("sportsStore")
-    //.constant("dataUrl", "http://localhost:5500/products")
     .controller("sportsStoreCtrl", function ($scope, JsonService) {
         $scope.data = JsonService.getData();
     });
